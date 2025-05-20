2d8beeefbbcb572309493204c4169a4b970aabe5
Chinmay <chinmaybhatk@gmail.com>
2025-05-20 21:00:50 +0530

    Refactor cashfree module initialization and import submodules

--- cashfree/__init__.py
+++ cashfree/__init__.py
@@ -1,4 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-__version__ = "0.0.1"
+__version__ = '0.0.1'
+
+# Import the cashfree module directly
+import os
+import sys
+
+# Add the current directory to the path
+current_dir = os.path.dirname(os.path.abspath(__file__))
+if current_dir not in sys.path:
+    sys.path.insert(0, current_dir)
+
+# Import cashfree module
+import cashfree as _cashfree_module
+# Make it available as cashfree.cashfree
+sys.modules['cashfree.cashfree'] = _cashfree_module
--- cashfree/cashfree/__init__.py
+++ cashfree/cashfree/__init__.py
@@ -1,1 +1,10 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+# Import submodules to make them available
+from . import controllers
+# If you have an api.py in this directory, also import that
+try:
+    from . import api
+except ImportError:
+    pass
